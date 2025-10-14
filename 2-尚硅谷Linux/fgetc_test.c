94eefc87c598f55e96a1acc4c606ed88aa0f28d6
Sephoration <Epliskey@foxmail.com>
2025-10-14 21:47:05 +0800

    1

---
+++ 2-尚硅谷Linux/fgetc_test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    //打开文件
+    FILE *fp = fopen ("test.txt", "r");
+    if (fp == NULL)
+    {
+        perror ("fopen 失败！");
+        return -1;
+    }
+    else
+    {
+        printf ("fopen 成功！\n");
+    }
+
+
+    //读取文件内容
+    /*
+    FILE *__stream : 文件指针
+    返回值： 成功返回读取的字符，失败返回EOF
+    int fgetc (FILE *__stream);
+    */
+    char c = fgetc(fp); //读取一个字符
+    while ( c != EOF )  //只要没有读到文件结尾，就一直读取
+    {
+        printf ("%c", c); //输出读取的字符
+        c = fgetc(fp);    //继续读取下一个字符
+    }
+    printf ("\n");
+  
+
+    // 关闭文件
+    int close_ret = fclose (fp);
+    if (close_ret == 0)
+    {
+        printf ("fclose 成功！\n");
+    }
+    else
+    {
+        perror ("fclose 失败！");   
+    }
+
+    return 0;    
+}
---
+++ 2-尚硅谷Linux/fputc_test.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(int argc,char const *argv[]) {
+
+    /*
+   const char *__restrict __filename  #字符串表示打开文件
+   const char *__restrict __modes     #字符串表示打开文件的模式
+        1. r:以只读方式打开文件,文件必须存在
+        2. w:以只写方式打开文件,文件不存在则创建,存在
+        3. a:以追加方式打开文件,文件不存在则创建,存在
+        4. r+:以读写方式打开文件,文件必须存在
+        5. w+:以读写方式打开文件,文件不存在则创建,存在则清空
+        6. a+:以读写方式打开文件,文件不存在则
+        7. b:以二进制方式打开文件
+        8. t:以文本方式打开文件(默认)
+        9. +:读写方式
+    return: FILE * 结构体指针，表示一个文件
+        报错返回NULL
+    FILE *fopen (const char *__restrict __filename , const char *__restrict __modes)
+    */
+    char *filename = "test.txt";
+    char *modes = "w+";
+    FILE *fp = fopen(filename,modes);
+    if(fp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    printf("fopen 成功打开！\n");
+
+
+    /*
+    int __c  ASCII码值对应的char
+    FILE *__stream 文件指针
+    return: 成功返回写入的字符，失败返回EOF
+    int fputc (int __c, FILE *__stream);
+    */
+    int put_result = fputc(97, fp);
+    if (put_result == EOF)
+    {
+        perror("fputc");
+        return -1;
+    }
+    else {
+        printf("fputc 成功写入！\n");
+    }
+
+
+
+    /* 
+    FILE *__stream #文件指针
+    return: 成功返回0，失败返回EOF
+    int fclose (FILE *__stream);
+    */
+   int result = fclose(fp);  // 关闭文件
+    if(result == EOF) {
+         perror("fclose");
+         return -1;
+    }
+    else {
+        printf("fclose 成功关闭！\n");
+    }
+ 
+}
